975a53765bf5c259c49763993dd49b81b752637a
trivo <voductri744464@gmail.com>
2024-04-26 16:08:17 +0700

    Tin nhắn 2 người á nhá tạm thời vậy

---
+++ SingalRCHAT/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+namespace SingalRCHAT
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            string currentTime = DateTime.Now.ToLocalTime().ToString(); // Lấy thời gian hiện tại
+            string messageWithTime = $"[{currentTime}] {user}: {message}"; // Thêm thời gian vào tin nhắn
+            await Clients.All.SendAsync("ReceiveMessage", user, messageWithTime);
+
+        }
+    }
+}
---
+++ SingalRCHAT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SingalRCHAT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
